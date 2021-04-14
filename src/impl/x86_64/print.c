8074352ac8a188401e7e1832df4d8615f976f2b0
fatimanaeemm <62249250+fatimanaeemm@users.noreply.github.com>
2021-04-14 20:36:15 +0500

    Update print.c

--- src/impl/x86_64/print.c
+++ src/impl/x86_64/print.c
@@ -1,3 +1,88 @@
+#include "print.h"
+
+const static size_t NUM_COLS = 80;
+const static size_t NUM_ROWS = 25;
+const int height = 5;
+
+struct Char {
+    uint8_t character;
+    uint8_t color;
+};
+
+struct Char* buffer = (struct Char*) 0xb8000;
+size_t col = 0;
+size_t row = 0;
+uint8_t color = PRINT_COLOR_WHITE | PRINT_COLOR_BLACK << 4;
+
+void clear_row(size_t row) {
+    struct Char empty = (struct Char) {
+        character: ' ',
+        color: color,
+    };
+
+    for (size_t col = 0; col < NUM_COLS; col++) {
+        buffer[col + NUM_COLS * row] = empty;
+    }
+}
+
+void print_clear() {
+    for (size_t i = 0; i < NUM_ROWS; i++) {
+        clear_row(i);
+    }
+}
+
+void print_newline() {
+    col = 0;
+
+    if (row < NUM_ROWS - 1) {
+        row++;
+        return;
+    }
+
+    for (size_t row = 1; row < NUM_ROWS; row++) {
+        for (size_t col = 0; col < NUM_COLS; col++) {
+            struct Char character = buffer[col + NUM_COLS * row];
+            buffer[col + NUM_COLS * (row - 1)] = character;
+        }
+    }
+
+    clear_row(NUM_COLS - 1);
+}
+
+void print_char(char character) {
+    if (character == '\n') {
+        print_newline();
+        return;
+    }
+
+    if (col > NUM_COLS) {
+        print_newline();
+    }
+
+    buffer[col + NUM_COLS * row] = (struct Char) {
+        character: (uint8_t) character,
+        color: color,
+    };
+
+    col++;
+}
+
+void print_str(char* str) {
+    int i, j;
+    for (i = 0; i < height; i++) {
+        printS(i,j);
+        giveSpace();
+        printE(i,j);
+        giveSpace();
+        printE(i,j);
+        giveSpace();
+        printC(i,j);
+        giveSpace();
+        printS(i,j);
+        print_newline();
+    }
+}
+
 void printS(int i, int j)
 {
     for (j = 0; j < height; j++) {
